161be1a107ccc0e3a1f0f0ca9f27d28a248d5b61
chirag-logical <88182006+chirag-logical@users.noreply.github.com>
2022-07-15 23:56:59 +0530

    Update api.js
    Co-authored-by: Chirag Mehta<chiragmehta900@gmail.com>

--- src/configs/api.js
+++ src/configs/api.js
@@ -1,8 +1,10 @@
 // AWS SERVER API
 const API = "http://217.172.172.124:4000/api/v1/"
 
+
 //TESTING SERVER
 // const API = "http://localhost:4000/api/v1/"
+
 
 //LOCAL
 // const API = "http://localhost:4000/api/v1/"
